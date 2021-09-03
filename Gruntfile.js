da35aac37690c69aa235d50484e7effb2ede17fc
Triin <triin.kask@industry62.com>
2021-09-03 16:14:50 +0300

    Gruntfile Angular version for deployment

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+// Generated on 2015-07-11 using
+// generator-webapp 1.0.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// If you want to recursively match all subfolders, use:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-build-control');
+  var pkg = require('./package.json');
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:TriinK/vimac.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
+    }
+  })
+}
